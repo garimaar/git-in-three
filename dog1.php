996abd62be176851af8512510db1eaff2c8cc95a
garima <garimaarora121999@gmail.com>
2021-06-24 16:14:48 +0530

    all files saved and modified

--- dog1.php
+++ dog1.php
@@ -3,7 +3,7 @@ namespace dog1;
 class dog {
   public $breed = " ";
   public function breed() {
-    echo "<p>dog1 breed is {$this->breed}.</p>";
+    echo "<p>dog breed is {$this->breed}.</p>";
   }
 }
 ?>
--- dog2.php
+++ dog2.php
@@ -3,7 +3,7 @@ namespace dog2;
 class dog {
   public $name = " ";
   public function name() {
-    echo "<p>dog2 name is {$this->name}.</p>";
+    echo "<p>dog name is {$this->name}.</p>";
   }
 }
 ?>
